763c12bc84b0c9e249f31e2c8a1c9f5514b641fb
Odd E. Ebbesen <oddebb@gmail.com>
2021-01-21 23:29:43 +0100

    Created alternative layout for comparing drinks. Works almost OK if not too long drink names, or in landscape mode. Added android:configChanges to manifest. Now orientation change is almost OK, except for when viewing the drink list.

--- app/src/main/java/net/oddware/alcolator/CompareDrinksFragment.kt
+++ app/src/main/java/net/oddware/alcolator/CompareDrinksFragment.kt
@@ -1,13 +1,15 @@
 package net.oddware.alcolator
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import kotlinx.android.synthetic.main.fragment_compare_drinks.view.*
+import kotlinx.android.synthetic.main.fragment_compare_drinks_alt1.view.*
 import timber.log.Timber
 import java.util.*
 
@@ -21,7 +23,7 @@ class CompareDrinksFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_compare_drinks, container, false)
+        val view = inflater.inflate(R.layout.fragment_compare_drinks_alt1, container, false)
 
         drink1ItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(
@@ -78,6 +80,33 @@ class CompareDrinksFragment : Fragment() {
         }
         view.spnCmpDrink2Chooser.onItemSelectedListener = drink2ItemSelectedListener
 
+        view.etCmpDrink1AmountVal.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                //Timber.d("Amount: $s, length: $count")
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                //Timber.d("Text was changed: ${s.toString()}")
+                s.toString().toIntOrNull() ?: return
+                drinkAdapter?.let { da ->
+                    val d1pos = view.spnCmpDrink1Chooser.selectedItemPosition
+                    val d2pos = view.spnCmpDrink2Chooser.selectedItemPosition
+
+                    if (d1pos > 0) {
+                        val d1 = da.getItem(d1pos)
+                        displayDrink1(view, d1)
+                        if (d2pos > 0) {
+                            val d2 = da.getItem(d2pos)
+                            displayDrink2(view, d1, d2)
+                        }
+                    }
+                }
+            }
+        })
+
         return view
     }
 
@@ -131,19 +160,25 @@ class CompareDrinksFragment : Fragment() {
 
     private inline fun fmtdbl(d: Double): String = String.format(Locale.US, "%.2f", d)
 
+    private fun getD1Amount(v: View): Int {
+        return v.etCmpDrink1AmountVal.text.toString().toIntOrNull() ?: 1
+    }
+
     private fun displayDrink1(v: View, drink: Drink?) {
         if (null == drink) {
             v.tvCmpDrink1PctVal.text = null
             v.tvCmpDrink1VolVal.text = null
             v.tvCmpDrink1AlcVolVal.text = null
             v.tvCmpDrink1PriceVal.text = null
-            v.tvCmpDrink1PricePMLAVal.text = null
+            //v.tvCmpDrink1PricePMLAVal.text = null
+            v.spnCmpDrink2Chooser.setSelection(0)
         } else {
+            val amount = getD1Amount(v)
             v.tvCmpDrink1PctVal.text = fmtdbl(drink.alcPct)
-            v.tvCmpDrink1VolVal.text = drink.volumeML.toString()
-            v.tvCmpDrink1AlcVolVal.text = fmtdbl(drink.alcML())
-            v.tvCmpDrink1PriceVal.text = fmtdbl(drink.price)
-            v.tvCmpDrink1PricePMLAVal.text = fmtdbl(drink.pricePerAlcML())
+            v.tvCmpDrink1VolVal.text = (drink.volumeML * amount).toString()
+            v.tvCmpDrink1AlcVolVal.text = fmtdbl(drink.alcML() * amount)
+            v.tvCmpDrink1PriceVal.text = fmtdbl(drink.price * amount)
+            //v.tvCmpDrink1PricePMLAVal.text = fmtdbl(drink.pricePerAlcML())
         }
     }
 
@@ -154,20 +189,21 @@ class CompareDrinksFragment : Fragment() {
             v.tvCmpDrink2VolVal.text = null
             v.tvCmpDrink2AlcVolVal.text = null
             v.tvCmpDrink2PriceVal.text = null
-            v.tvCmpDrink2PricePMLAVal.text = null
+            //v.tvCmpDrink2PricePMLAVal.text = null
             return
         }
         val mres = d1.matchDrink(d2)
+        val amount = getD1Amount(v)
         v.tvCmpDrink2PctVal.text = fmtdbl(d2.alcPct)
         v.tvCmpDrink2AmountVal.text = String.format(
             Locale.US,
             "%.2f X %d ml",
-            mres.times,
+            mres.times * amount,
             d2.volumeML
         )
-        v.tvCmpDrink2VolVal.text = fmtdbl(mres.ml)
-        v.tvCmpDrink2AlcVolVal.text = fmtdbl(d2.alcML() * mres.times)
-        v.tvCmpDrink2PriceVal.text = fmtdbl(mres.times * d2.price)
-        v.tvCmpDrink2PricePMLAVal.text = fmtdbl(d2.pricePerAlcML())
+        v.tvCmpDrink2VolVal.text = fmtdbl(mres.ml * amount)
+        v.tvCmpDrink2AlcVolVal.text = fmtdbl(d2.alcML() * mres.times * amount)
+        v.tvCmpDrink2PriceVal.text = fmtdbl(mres.times * d2.price * amount)
+        //v.tvCmpDrink2PricePMLAVal.text = fmtdbl(d2.pricePerAlcML())
     }
 }

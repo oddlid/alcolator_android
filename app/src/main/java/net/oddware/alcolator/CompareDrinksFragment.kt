9e0b99513f61fceddae4221db2ccd6ce9dd23a3b
Odd E. Ebbesen <oddebb@gmail.com>
2021-01-21 21:25:53 +0100

    Made a mostly working Drink Comparator. Will try to create a more intuitive layout.

--- app/src/main/java/net/oddware/alcolator/CompareDrinksFragment.kt
+++ app/src/main/java/net/oddware/alcolator/CompareDrinksFragment.kt
@@ -4,14 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import kotlinx.android.synthetic.main.fragment_compare_drinks.view.*
 import timber.log.Timber
+import java.util.*
 
 class CompareDrinksFragment : Fragment() {
     private var drinkAdapter: DrinkArrayAdapter? = null
     private lateinit var drinkViewModel: DrinkViewModel
+    private lateinit var drink1ItemSelectedListener: AdapterView.OnItemSelectedListener
+    private lateinit var drink2ItemSelectedListener: AdapterView.OnItemSelectedListener
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,11 +23,60 @@ class CompareDrinksFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_compare_drinks, container, false)
 
-        //drinkAdapter?.let {
-        //    it.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        //    view.spnCmpDrink1Chooser.adapter = drinkAdapter
-        //    view.spnCmpDrink2Chooser.adapter = drinkAdapter
-        //}
+        drink1ItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                v: View?,
+                position: Int,
+                id: Long
+            ) {
+                if (0 == position) {
+                    displayDrink1(view, null)
+                } else {
+                    drinkAdapter?.let {
+                        val d1 = it.getItem(position)
+                        displayDrink1(view, it.getItem(position))
+                        val d2pos = view.spnCmpDrink2Chooser.selectedItemPosition
+                        if (0 != d2pos) {
+                            val d2 = it.getItem(d2pos)
+                            displayDrink2(view, d1, d2)
+                        }
+                    }
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                Timber.d("Nothing selected for Drink #1 spinner")
+            }
+        }
+        view.spnCmpDrink1Chooser.onItemSelectedListener = drink1ItemSelectedListener
+
+        drink2ItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                v: View?,
+                position: Int,
+                id: Long
+            ) {
+                if (0 == position) {
+                    displayDrink2(view, null, null)
+                } else {
+                    drinkAdapter?.let {
+                        val d1pos = view.spnCmpDrink1Chooser.selectedItemPosition
+                        if (0 != d1pos) {
+                            val d1 = it.getItem(d1pos)
+                            val d2 = it.getItem(position)
+                            displayDrink2(view, d1, d2)
+                        }
+                    }
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                Timber.d("Nothing selected for Drink #2 spinner")
+            }
+        }
+        view.spnCmpDrink2Chooser.onItemSelectedListener = drink2ItemSelectedListener
 
         return view
     }
@@ -35,21 +88,86 @@ class CompareDrinksFragment : Fragment() {
 
         drinkViewModel.drinks.observe(viewLifecycleOwner, { drinks ->
             if (null != drinks) {
-                // assign adapter
-                Timber.d("Assigning DrinkArrayAdapter...")
-                drinkAdapter = context?.let { ctx ->
-                    DrinkArrayAdapter(
+                // Insert inactive header selection
+                val mdrinks = drinks.toMutableList()
+                context?.let { ctx ->
+                    mdrinks.add(
+                        0,
+                        Drink(
+                            name = ctx.getString(R.string.cmpDrinkSpnHdr),
+                            tag = "--"
+                        ) // bogus header
+                    )
+                    // Create adapter
+                    drinkAdapter = DrinkArrayAdapter(
                         ctx,
                         android.R.layout.simple_spinner_item,
                         0,
-                        drinks.toMutableList()
+                        mdrinks
                     ).also { it.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item) }
                 }
+
+                // If we would like to override a method in DrinkArrayAdapter, we could assign it
+                // like this instead of the above:
+                //drinkAdapter = object : DrinkArrayAdapter(
+                //    requireContext(),
+                //    android.R.layout.simple_spinner_item,
+                //    0,
+                //    mdrinks
+                //) {
+                //    override fun isEnabled(position: Int): Boolean {
+                //        return position != 0
+                //    }
+                //}.also { it.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item) }
+
+
                 view?.let {
                     it.spnCmpDrink1Chooser.adapter = drinkAdapter
                     it.spnCmpDrink2Chooser.adapter = drinkAdapter
                 }
             }
         })
+    }
+
+    private inline fun fmtdbl(d: Double): String = String.format(Locale.US, "%.2f", d)
+
+    private fun displayDrink1(v: View, drink: Drink?) {
+        if (null == drink) {
+            v.tvCmpDrink1PctVal.text = null
+            v.tvCmpDrink1VolVal.text = null
+            v.tvCmpDrink1AlcVolVal.text = null
+            v.tvCmpDrink1PriceVal.text = null
+            v.tvCmpDrink1PricePMLAVal.text = null
+        } else {
+            v.tvCmpDrink1PctVal.text = fmtdbl(drink.alcPct)
+            v.tvCmpDrink1VolVal.text = drink.volumeML.toString()
+            v.tvCmpDrink1AlcVolVal.text = fmtdbl(drink.alcML())
+            v.tvCmpDrink1PriceVal.text = fmtdbl(drink.price)
+            v.tvCmpDrink1PricePMLAVal.text = fmtdbl(drink.pricePerAlcML())
+        }
+    }
+
+    private fun displayDrink2(v: View, d1: Drink?, d2: Drink?) {
+        if (null == d1 || null == d2) {
+            v.tvCmpDrink2PctVal.text = null
+            v.tvCmpDrink2AmountVal.text = null
+            v.tvCmpDrink2VolVal.text = null
+            v.tvCmpDrink2AlcVolVal.text = null
+            v.tvCmpDrink2PriceVal.text = null
+            v.tvCmpDrink2PricePMLAVal.text = null
+            return
+        }
+        val mres = d1.matchDrink(d2)
+        v.tvCmpDrink2PctVal.text = fmtdbl(d2.alcPct)
+        v.tvCmpDrink2AmountVal.text = String.format(
+            Locale.US,
+            "%.2f X %d ml",
+            mres.times,
+            d2.volumeML
+        )
+        v.tvCmpDrink2VolVal.text = fmtdbl(mres.ml)
+        v.tvCmpDrink2AlcVolVal.text = fmtdbl(d2.alcML() * mres.times)
+        v.tvCmpDrink2PriceVal.text = fmtdbl(mres.times * d2.price)
+        v.tvCmpDrink2PricePMLAVal.text = fmtdbl(d2.pricePerAlcML())
     }
 }
--- app/src/main/java/net/oddware/alcolator/DrinkArrayAdapter.kt
+++ app/src/main/java/net/oddware/alcolator/DrinkArrayAdapter.kt
@@ -1,6 +1,8 @@
 package net.oddware.alcolator
 
 import android.content.Context
+import android.content.res.Resources
+import android.view.ContextThemeWrapper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,20 +11,31 @@ import android.widget.TextView
 import timber.log.Timber
 import java.util.*
 
-class DrinkArrayAdapter(
+open class DrinkArrayAdapter(
     private val ctx: Context,
     private val resource: Int,
     private val textViewResourceId: Int,
     private val drinks: MutableList<Drink>
 ) : ArrayAdapter<Drink>(ctx, resource, textViewResourceId, drinks) {
 
-    // This seems to only set the text for the selected item.
-    // TODO: find a way to do the same for items in list when clicking dropdown
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        // The code here is mostly just translated from ArrayAdapter Java, and customized
-        // at the end to get the desired text from a Drink object
+    /*
+    This class mostly repeats what its superclass does, but separates out setting the text, so that
+    we get custom text from the Drink item.
+     */
+
+    private val mInflater = LayoutInflater.from(ctx)
+    private var mDropDownInflater: LayoutInflater? = null
+    private var mResource = resource
+    private var mDropDownResource = resource
+
+    private fun createTextViewFromResource(
+        inflater: LayoutInflater,
+        convertView: View?,
+        parent: ViewGroup,
+        resId: Int
+    ): TextView {
         val txtView: TextView
-        val view: View = convertView ?: LayoutInflater.from(ctx).inflate(resource, parent, false)
+        val view: View = convertView ?: inflater.inflate(resId, parent, false)
 
         try {
             if (0 == textViewResourceId) {
@@ -45,7 +58,10 @@ class DrinkArrayAdapter(
             throw IllegalStateException("ArrayAdapter requires the resource ID to be a TextView", e)
         }
 
-        val drink = getItem(position)
+        return txtView
+    }
+
+    private fun setDrinkText(txtView: TextView, drink: Drink?) {
         if (null != drink) {
             txtView.text = String.format(
                 Locale.US,
@@ -56,7 +72,51 @@ class DrinkArrayAdapter(
         } else {
             txtView.text = "NULL DRINK!"
         }
+    }
 
-        return view
+    // TODO: think through if this is how we want it
+    // We could subclass and override from call site instead
+    //override fun isEnabled(position: Int): Boolean {
+    //    // Since we insert a header at position 0 before using this adapter, we will not enable
+    //    // the item at this position.
+    //    // This will make item #0 appear as selected when loading the list, but it will not be
+    //    // possible to select that item after picking another selection.
+    //    // Maybe this is not what we really want later. Could be just as well to check for position 0
+    //    // in the listener and clear UI when selecting item #0 instead....
+    //    return position != 0
+    //}
+
+    override fun setDropDownViewResource(resId: Int) {
+        super.setDropDownViewResource(resId) // need to let the parent set its own mDropDownResource
+        mDropDownResource = resId
+    }
+
+    override fun setDropDownViewTheme(theme: Resources.Theme?) {
+        super.setDropDownViewTheme(theme) // make sure the parent sets its own internal variables
+        // just repeat what super does, so we get a hold of our own dropDownInflater
+        if (null == theme) {
+            mDropDownInflater = null
+        } else if (mInflater.context.theme == theme) {
+            mDropDownInflater = mInflater
+        } else {
+            mDropDownInflater = LayoutInflater.from(ContextThemeWrapper(ctx, theme))
+        }
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val txtView = createTextViewFromResource(mInflater, convertView, parent, mResource)
+        setDrinkText(txtView, getItem(position))
+        return txtView
+    }
+
+    override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val txtView = createTextViewFromResource(
+            mDropDownInflater ?: mInflater,
+            convertView,
+            parent,
+            mDropDownResource,
+        )
+        setDrinkText(txtView, getItem(position))
+        return txtView
     }
 }
